8fb9cc34b5ff1386f23d463767a2c2e2de11382c
seubpong.mon <pjame.fb@gmail.com>
2022-03-16 20:36:07 +0700

    Rename service account

--- 03-sa.bash
+++ 03-sa.bash
@@ -2,7 +2,7 @@
 
 source .env
 
-gcloud iam service-accounts create ${SA_NAME} --project ${PROJECT} --display-name="Service account for Cortex"
+gcloud iam service-accounts create ${SA_NAME} --project ${PROJECT} --display-name="Service account for Loki"
 
 gcloud projects add-iam-policy-binding ${PROJECT} \
     --member="serviceAccount:${SA_NAME}@${PROJECT}.iam.gserviceaccount.com" \
