570e5ab8a8fd97316dcf0cc1a8abb3722b3737df
seubpong.mon <pjame.fb@gmail.com>
2022-03-16 20:05:18 +0700

    Added project option

--- 01-setup-gke.bash
+++ 01-setup-gke.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcloud container clusters create gke-loki --zone us-central1-a
+gcloud container clusters create gke-loki --zone us-central1-a --project ${PROJECT}
 
-gcloud container clusters get-credentials gke-loki --zone us-central1-a
+gcloud container clusters get-credentials gke-loki --zone us-central1-a --project ${PROJECT}
 
 kubectl get nodes
